5aff84230200b6866dd0c4f8f7027cf1f9fc10eb
TZ <t.zhang@nki.nl>
2022-12-15 20:38:50 +0100

    Create Attention_layers.py

---
+++ Attention_layers.py
@@ -0,0 +1,128 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras
+from tensorflow.keras import backend as K
+import tensorflow.keras.layers as KL
+from tensorflow.keras.layers import InputSpec
+
+def hw_flatten(x):
+    return K.reshape(x, shape=[K.shape(x)[0], K.shape(x)[1]*K.shape(x)[2], K.shape(x)[-1]])
+    
+class Self_Attention(tf.keras.layers.Layer):
+
+    def __init__(self, ch, **kwargs):
+        super(Self_Attention, self).__init__(**kwargs)
+        self.channels = ch
+        self.filters_f_g = self.channels // 8
+        self.filters_h = self.channels
+
+    def build(self, input_shape):
+        kernel_shape_f_g = (1, 1) + (self.channels, self.filters_f_g)
+        print(kernel_shape_f_g)
+        kernel_shape_h = (1, 1) + (self.channels, self.filters_h)
+
+        self.gamma = self.add_weight(name='gamma', shape=[1], initializer='zeros', trainable=True)
+        self.kernel_f = self.add_weight(shape=kernel_shape_f_g,
+                                        initializer='glorot_uniform',
+                                        name='kernel_f')
+        self.kernel_g = self.add_weight(shape=kernel_shape_f_g,
+                                        initializer='glorot_uniform',
+                                        name='kernel_g')
+        self.kernel_h = self.add_weight(shape=kernel_shape_h,
+                                        initializer='glorot_uniform',
+                                        name='kernel_h')
+        self.bias_f = self.add_weight(shape=(self.filters_f_g,),
+                                      initializer='zeros',
+                                      name='bias_F')
+        self.bias_g = self.add_weight(shape=(self.filters_f_g,),
+                                      initializer='zeros',
+                                      name='bias_g')
+        self.bias_h = self.add_weight(shape=(self.filters_h,),
+                                      initializer='zeros',
+                                      name='bias_h')
+        super(Self_Attention, self).build(input_shape)
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=4,
+                                    axes={3: input_shape[-1]})
+        self.built = True
+
+
+    def call(self, x):
+        def hw_flatten(x):
+            return K.reshape(x, shape=[K.shape(x)[0], K.shape(x)[1]*K.shape(x)[2], K.shape(x)[-1]])
+        
+        f = K.conv2d(x,
+                     kernel=self.kernel_f,
+                     strides=(1, 1), padding='same')  # [bs, h, w, c']
+        f = K.bias_add(f, self.bias_f)
+        g = K.conv2d(x,
+                     kernel=self.kernel_g,
+                     strides=(1, 1), padding='same')  # [bs, h, w, c']
+        g = K.bias_add(g, self.bias_g)
+        h = K.conv2d(x,
+                     kernel=self.kernel_h,
+                     strides=(1, 1), padding='same')  # [bs, h, w, c]
+        h = K.bias_add(h, self.bias_h)
+
+        s = tf.matmul(hw_flatten(g), hw_flatten(f), transpose_b=True)  # # [bs, N, N]
+
+        beta = K.softmax(s, axis=-1)  # attention map
+
+        o = K.batch_dot(beta, hw_flatten(h))  # [bs, N, C]
+
+        o = K.reshape(o, shape=K.shape(x))  # [bs, h, w, C]
+        x = self.gamma * o + x
+        
+        return x
+       
+    def get_config(self):
+
+        config=super().get_config().copy()
+        config.update({
+            'ch': self.channels,
+            'self.channels // 1': self.filters_f_g,
+            'self.channels': self.filters_h,
+        })
+        return config
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+      
+channel_axis = 1 if K.image_data_format() == "channels_first" else 3
+
+# channel_attention
+def channel_attention(input_xs, reduction_ratio=0.125):
+    # get channel
+    channel = int(input_xs.shape[channel_axis])
+    maxpool_channel = KL.GlobalMaxPooling2D()(input_xs)
+    maxpool_channel = KL.Reshape((1, 1, channel))(maxpool_channel)
+    avgpool_channel = KL.GlobalAvgPool2D()(input_xs)
+    avgpool_channel = KL.Reshape((1, 1, channel))(avgpool_channel)
+    Dense_One = KL.Dense(units=int(channel * reduction_ratio), activation='relu', kernel_initializer='he_normal', use_bias=True, bias_initializer='zeros')
+    Dense_Two = KL.Dense(units=int(channel), activation='relu', kernel_initializer='he_normal', use_bias=True, bias_initializer='zeros')
+
+    mlp_1_max = Dense_One(maxpool_channel)
+    mlp_2_max = Dense_Two(mlp_1_max)
+    mlp_2_max = KL.Reshape(target_shape=(1, 1, int(channel)))(mlp_2_max)
+
+    mlp_1_avg = Dense_One(avgpool_channel)
+    mlp_2_avg = Dense_Two(mlp_1_avg)
+    mlp_2_avg = KL.Reshape(target_shape=(1, 1, int(channel)))(mlp_2_avg)
+    channel_attention_feature = KL.Add()([mlp_2_max, mlp_2_avg])
+    channel_attention_feature = KL.Activation('sigmoid')(channel_attention_feature)
+    return KL.Multiply()([channel_attention_feature, input_xs])
+
+# spatial_attention
+def spatial_attention(channel_refined_feature):
+    maxpool_spatial = KL.Lambda(lambda x: K.max(x, axis=3, keepdims=True))(channel_refined_feature)
+    avgpool_spatial = KL.Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(channel_refined_feature)
+    max_avg_pool_spatial = KL.Concatenate(axis=3)([maxpool_spatial, avgpool_spatial])
+    return KL.Conv2D(filters=1, kernel_size=(3, 3), padding="same", activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(max_avg_pool_spatial)
+
+# channel_spatial_attention
+def CSA(input_xs, reduction_ratio=0.5):
+    channel_refined_feature = channel_attention(input_xs, reduction_ratio=reduction_ratio)
+    spatial_attention_feature = spatial_attention(channel_refined_feature)
+    refined_feature = KL.Multiply()([channel_refined_feature, spatial_attention_feature])
+    return KL.Add()([refined_feature, input_xs])
+  
