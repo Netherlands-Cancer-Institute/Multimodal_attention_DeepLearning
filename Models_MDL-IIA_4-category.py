7754d88559277b8bdaaea5839c018f689168b6ec
TZ <t.zhang@nki.nl>
2022-12-15 20:44:34 +0100

    Update Models_MDL-IIA_4-category.py

--- Models_MDL-IIA_4-category.py
+++ Models_MDL-IIA_4-category.py
@@ -18,7 +18,7 @@ from sklearn import metrics, manifold
 from sklearn.metrics import matthews_corrcoef  
 from sklearn.model_selection import train_test_split
 from Data_loading import read_mg, read_us
-from Attention_layers import Self_Attention, CSA
+from Attention_layers import Self_Attention
 
 ### set seed
 tf.random.set_seed(1203)
@@ -96,6 +96,41 @@ def get_lr_metric(optimizer):
     def lr(y_true, y_pred):
         return optimizer.lr
     return lr
+
+### channel_spatial_attention
+channel_axis = 1 if K.image_data_format() == "channels_first" else 3
+def channel_attention(input_xs, reduction_ratio=0.125):
+    # get channel
+    channel = int(input_xs.shape[channel_axis])
+    maxpool_channel = KL.GlobalMaxPooling2D()(input_xs)
+    maxpool_channel = KL.Reshape((1, 1, channel))(maxpool_channel)
+    avgpool_channel = KL.GlobalAvgPool2D()(input_xs)
+    avgpool_channel = KL.Reshape((1, 1, channel))(avgpool_channel)
+    Dense_One = KL.Dense(units=int(channel * reduction_ratio), activation='relu', kernel_initializer='he_normal', use_bias=True, bias_initializer='zeros')
+    Dense_Two = KL.Dense(units=int(channel), activation='relu', kernel_initializer='he_normal', use_bias=True, bias_initializer='zeros')
+
+    mlp_1_max = Dense_One(maxpool_channel)
+    mlp_2_max = Dense_Two(mlp_1_max)
+    mlp_2_max = KL.Reshape(target_shape=(1, 1, int(channel)))(mlp_2_max)
+
+    mlp_1_avg = Dense_One(avgpool_channel)
+    mlp_2_avg = Dense_Two(mlp_1_avg)
+    mlp_2_avg = KL.Reshape(target_shape=(1, 1, int(channel)))(mlp_2_avg)
+    channel_attention_feature = KL.Add()([mlp_2_max, mlp_2_avg])
+    channel_attention_feature = KL.Activation('sigmoid')(channel_attention_feature)
+    return KL.Multiply()([channel_attention_feature, input_xs])
+def spatial_attention(channel_refined_feature):
+    maxpool_spatial = KL.Lambda(lambda x: K.max(x, axis=3, keepdims=True))(channel_refined_feature)
+    avgpool_spatial = KL.Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(channel_refined_feature)
+    max_avg_pool_spatial = KL.Concatenate(axis=3)([maxpool_spatial, avgpool_spatial])
+    return KL.Conv2D(filters=1, kernel_size=(3, 3), padding="same", activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(max_avg_pool_spatial)
+
+def CSA(input_xs, reduction_ratio=0.5):
+    channel_refined_feature = channel_attention(input_xs, reduction_ratio=reduction_ratio)
+    spatial_attention_feature = spatial_attention(channel_refined_feature)
+    refined_feature = KL.Multiply()([channel_refined_feature, spatial_attention_feature])
+    return KL.Add()([refined_feature, input_xs])
+
   
 ### Model
 base1=ResNet50(weights='imagenet',include_top=False,input_shape=(256, 256, 3))
